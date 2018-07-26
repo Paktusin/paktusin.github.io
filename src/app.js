b9cc90e36fbf56702b48818f29390a32e505a539
davlichin.ma <davlichin.ma@talantiuspeh.ru>
2018-07-26 12:36:12 +0300

    start angular

--- src/app.js
+++ src/app.js
@@ -8,14 +8,15 @@ import projectsTemplate from './view/projects.html'
 import skillsTemplate from './view/skills.html'
 import bgs from "./bgs";
 import bG from "./components/bG/bG";
+import moment from 'moment';
 
-
-const app = angular.module('app', [uiRouter, mainNav.name, bG.name])
+angular.module('app', [uiRouter, mainNav.name, bG.name])
     .config(($stateProvider, $urlRouterProvider) => {
         $stateProvider
             .state('about', {
                 url: '/about',
                 template: aboutTemplate,
+                controller: 'aboutCtrl'
             })
             .state('education', {
                 url: '/education',
@@ -33,5 +34,24 @@ const app = angular.module('app', [uiRouter, mainNav.name, bG.name])
     })
     .controller('mainCtrl', ($rootScope, $scope) => {
         $scope.getBgs = () => Object.keys(bgs).map(key => bgs[key]);
+    })
+    .controller('aboutCtrl', ($interval, $scope) => {
+        const getCountDown = (date_string) => {
+            let res = [];
+            const now = moment();
+            let date = moment(date_string);
+            ['years', 'months', 'weeks', 'days', 'hours', 'minutes', 'seconds'].forEach(size => {
+                const count = now.diff(date, size);
+                date.subtract(-count, size);
+                res.push(count + ' ' + size);
+            });
+            return res.join(', ');
+        };
+        $scope.dev_time = 'Calculating...';
+        $scope.it_time = 'Calculating...';
+        $interval(() => {
+            $scope.dev_time = getCountDown('2015-09-01');
+            $scope.it_time = getCountDown('2012-01-01');
+        }, 1000);
     })
 ;
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     entry: {
@@ -48,6 +49,9 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: "[name].[hash].css",
         }),
+        new CopyWebpackPlugin([{
+            from: './src/public/', to: path.resolve(__dirname, 'dist'),
+        }])
     ],
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
