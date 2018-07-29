701b537c5cc94bcd7d2f26ea65fed47b342cd3cc
Mikahil <paktusin@gmail.com>
2018-07-29 11:33:26 +0300

    add tag

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import bgs from "./bgs";
 import bG from "./components/bG/bG";
 import moment from 'moment';
 
+console.log('hello!');
 
 angular.module('app', ['ui.router', mainNav.name, bG.name])
     .config(($stateProvider, $urlRouterProvider) => {
