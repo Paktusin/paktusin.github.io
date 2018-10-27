a3182ddeac7487e44ba24a69ab515bb8b8755f47
paktusin <paktusin@gmail.com>
2018-10-27 15:16:28 +0300

    vhs

--- src/app.js
+++ src/app.js
@@ -46,5 +46,22 @@ angular.module('app', ['ui.router', mainNav.name])
             $scope.dev_time = getCountDown('2015-09-01');
             $scope.it_time = getCountDown('2012-01-01');
         }, 1000);
+
+        function neonOn() {
+            $('.neon').addClass('on');
+        }
+
+        function brokeNeon() {
+            $('.neon').toggleClass('broke');
+        }
+        setTimeout(() => {
+            neonOn();
+            setInterval(() => {
+                brokeNeon();
+                setTimeout(() => {
+                    brokeNeon();
+                }, 2000);
+            }, 10000);
+        }, 1000);
     })
 ;
--- src/common.js
+++ src/common.js
@@ -1,13 +1,5 @@
 import moment from 'moment';
 
-function neonOn() {
-    $('.neon').addClass('on');
-}
-
-function brokeNeon() {
-    $('.neon').toggleClass('broke');
-}
-
 function updateTime() {
     $('#time').text(moment().format('YYYY-MM-DD HH:mm:ss'));
 }
@@ -36,13 +28,4 @@ $(() => {
     setInterval(updateGlitch, 50);
     setInterval(updateTime, 1000);
     createFloor();
-    setTimeout(() => {
-        neonOn();
-        setInterval(() => {
-            brokeNeon();
-            setTimeout(() => {
-                brokeNeon();
-            }, 2000);
-        }, 10000);
-    }, 1000);
 });
