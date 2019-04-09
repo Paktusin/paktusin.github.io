e42783cc1f968f49e26409312fd17963763bdd01
paktus <paktusin@gmail.com>
2019-04-09 11:57:00 +0300

    fucking cors

--- server/app.js
+++ server/app.js
@@ -15,9 +15,6 @@ app.use((req, res, next) => {
 });
 
 app.options('*', (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://paktusin.github.io/**");
-    res.header("Access-Control-Allow-Methods", "GET, POST");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.status(200).send('');
 });
 
