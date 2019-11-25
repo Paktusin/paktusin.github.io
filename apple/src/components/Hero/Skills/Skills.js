2ec8a22c02862fa58d32b066c281ee499cfa20f5
mikhail.davlichin <mikhail.davlichin@kaspersky.com>
2019-11-25 12:11:16 +0300

    redesign skills

--- apple/src/components/Hero/Skills/Skills.js
+++ apple/src/components/Hero/Skills/Skills.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Hero from "../Hero";
 import skills from "../../../common/skills";
 import {Container} from "../../Container/Container";
-import classes from "./Skills.module.scss"
+import classes, {image} from "./Skills.module.scss"
 import clsx from "clsx";
 
 export const Skills = () => {
@@ -10,16 +10,17 @@ export const Skills = () => {
         <Hero id={'skills'} className={classes.Skills}>
             <Container>
                 <div className={"row"}>
-                    {skills.frontend.map((skill,index) => (
-                        <div key={index} data-scroll={true} className={clsx("col-12 col-md-6 p-2", classes.SkillContainer)}>
-                            <div className={classes.Skill} style={{
-                                backgroundColor: skill.color,
-                                backgroundImage: `url(${skill.logo})`,
-                                color: (skill.textColor || 'white')
-                            }}>
-                                <div style={{maxWidth: 300}}>
+                    {skills.frontend.map((skill, index) => (
+                        <div key={index} data-scroll={true}
+                             className={clsx("col-12 col-md-6 p-3", classes.SkillContainer)}>
+                            <div className={classes.Skill}>
+                                <div>
+                                    <h2>{skill.name}</h2>
                                     <p>{skill.text}</p>
                                     {skill.more && <a target="_blank" href={skill.more}>Learn more</a>}
+                                </div>
+                                <div className={image}>
+                                    <div style={{backgroundImage: `url(${skill.logo})`}}></div>
                                 </div>
                             </div>
                         </div>
